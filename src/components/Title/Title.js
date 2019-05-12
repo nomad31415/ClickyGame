901c1eb8675cafc88c9905998f2a5a6da6a6be51
mcatoeff@gmail.com <mcatoeff@gmail.com>
2019-05-11 20:52:24 -0700

    added React App

---
+++ src/components/Title/Title.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Title.css";
+
+const Title = props => (
+    <div className="titlebox">
+        <h1>The Clicky Game!</h1>
+        <p>Click on an image to earn points, but do not click on same more than once!</p>
+    </div>
+);
+
+export default Title;
+
